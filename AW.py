47a740e00654d6dc651752f1d85445f5b0791abc
asdcxfrt <asdcxfrt@gmail.com>
2023-05-13 23:00:41 +0500

    Add Face PWGood

--- AW.py
+++ AW.py
@@ -4,55 +4,102 @@ import win32con
 import win32gui
 import pyaudio
 import audioop
+import math
+import time
+import numpy as np
 
-def get_loudness():
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-    data = stream.read(1024)
-    loudness = audioop.rms(data, 2)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    return loudness
+def move_window():
+    hwnd = py.display.get_wm_info()["window"]
+    # Получаем текущую позицию курсора на экране
+    x, y = win32api.GetCursorPos()
+    # Перемещаем окно по координатам курсора
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, x-350, y-350, 0, 0, win32con.SWP_NOSIZE)
+    
+
+def get_loudness(stream):
+  
+    data = stream.read(1024*3)
+    numpy_data = np.frombuffer(data, dtype=np.int16)
+    volume = np.abs(numpy_data).mean()
+    return volume
+
 py.init() 
-# initialize the pygame window
-window_screen = py.display.set_mode((700, 450),py.NOFRAME)
-# for borderless window use pygame.Noframe
-# size of the pygame window will be of width 700 and height 450
+
+window_screen = py.display.set_mode((700, 700),py.NOFRAME)
 hwnd = py.display.get_wm_info()["window"]
-# Getting information of the current active window
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(
                        hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(255, 0, 128), 0, win32con.LWA_COLORKEY)
-character = py.image.load("Sprites/head.png")
-mouthO = py.image.load("Sprites/open.png")
-mouthC = py.image.load("Sprites/close.png")
-done = 0
-while not done:  
-    # Accessing the event if any occurred
-    for event in py.event.get():   
-        # Checking if quit button is pressed or not
-        if event.type == py.QUIT:  
-            #  If quit then store true
-            done = 1               
-        # Checking if the escape button is pressed or not
-        if event.type == py.KEYDOWN:   
-            # If the escape button  is pressed then store true in the variable
-            if event.key == py.K_ESCAPE: 
-                done = 1
-     # Transparent background
-    window_screen.fill((255,0,128)) 
-    #  Calling the show_text function
-    window_screen.blit(character, (50, 50))
-    loudness = get_loudness()
+
+character = py.image.load("Sprites/PWGood1.png")
+mouth5 = py.image.load("Sprites/PWGood6.png")
+mouth4 = py.image.load("Sprites/PWGood5.png")
+mouth3 = py.image.load("Sprites/PWGood4.png")
+mouth2 = py.image.load("Sprites/PWGood3.png")
+mouth1 = py.image.load("Sprites/PWGood2.png")
+mouth=[character,mouth1,mouth2,mouth3,mouth4,mouth5]
+
+close = 0
+countframes=0
+old=0
+p = pyaudio.PyAudio()
+#stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+device_index = None
+for i in range(p.get_device_count()):
+    device_info = p.get_device_info_by_index(i)
+    #print(device_info)
+    if "VoiceMeeter" in device_info["name"]:
+        device_index = i
+        break
+
+if device_index is None:
+    print("Не удалось найти зацикленное звуковое устройство.")
+    exit()
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+# создание входного потока аудио
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                input_device_index=device_index,
+                frames_per_buffer=CHUNK_SIZE)
+maxV=2000
+t=time.time()
+window_screen.fill((255,0,128))
+window_screen.blit(character, (50, 50))
+while not close:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            close = True
+        elif event.type == py.MOUSEBUTTONDOWN:
+            
+            while py.mouse.get_pressed()[0]==True:
+                print(py.mouse.get_pressed()[0])
+                py.event.get()
+                # При нажатии на картинку вызываем функцию перемещения окна за мышью
+                move_window()
+    
+    loudness = get_loudness(stream)
     print(loudness)
-    if loudness > 1000:
-        window_screen.blit(mouthO, (50, 50))
-        mouthC.set_alpha(0)
-        mouthO.set_alpha(255)
+    if(loudness>maxV):
+        loudness=maxV
+    
+    Number_mouth=round(loudness/maxV*5)
+    print(Number_mouth)
+    for i in range(0,len(mouth)):
+        window_screen.blit(mouth[Number_mouth], (50, 50))
+        if(Number_mouth==i):
+            mouth[i].set_alpha(0)
+        else:
+            mouth[i].set_alpha(255)
     else:
-        window_screen.blit(mouthC, (50, 50))
-        mouthC.set_alpha(255)
-        mouthO.set_alpha(0)
-        
-    py.display.update()
+        #time.sleep(1/10)
+        py.display.update()
+stream.stop_stream()
+stream.close()
+p.terminate()
+py.quit()
