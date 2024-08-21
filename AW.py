fc5efe115da9592f3a3422e51b43b79bf85b1243
KonstPM <asdcxfrt@gmail.com>
2024-08-21 17:34:53 +0500

    Улучшил програмку

--- AW.py
+++ AW.py
@@ -3,17 +3,50 @@ import win32api
 import win32con
 import win32gui
 import pyaudio
-import audioop
-import math
+import os
 import time
 import numpy as np
+import json
+import sys
+
+# Размер окна
+Window_Size = 300
+# Повернуть персонажа в другую сторону 
+Reflect = True 
+# Режим VoiceMeeter.
+VoiceMeeterMode = False
+# Максимальная громкость входного аудиопотока. Если будет больше, то программа установит значение громкости равное maxV.
+maxV=2000
+# Вывод в консоль 
+DebugMode = True
+
+# Папка со спрайтами персонажа. Имена файлов должны легко сортироваться, чтобы при их упорядочивании они совпадали с вашими ожиданиями.
+# Чем больше цифра в имени файла, тем больше степень открытости рта, и т.д.
+sprite_folder = "Sprites"
+
+def LogLine(PrintLoudness=True, PrintSettings=False, PrintDeviceInfo=True):
+    global Window_Size, Reflect, VoiceMeeterMode
+    global loudness, maxV
+    if PrintLoudness:
+        print(f"Уровень громкости аудиопотока: {loudness}", end="")
+        if loudness>maxV:
+            print(f" Урезан как: {maxV}", end="")
+        print()
+    if PrintSettings:
+        print(f"Установленны настройки: Размер окна: {Window_Size}, Поворот персонажа: {Reflect}, Режим VoiceMeeter: {VoiceMeeterMode}, Верхний порог громкости {maxV}")
+    if PrintDeviceInfo:
+        json_string = json.dumps(device_info, indent=4)  
+        print(json_string)
+        
+    print("="*100)
+
 
 def move_window():
     hwnd = py.display.get_wm_info()["window"]
     # Получаем текущую позицию курсора на экране
     x, y = win32api.GetCursorPos()
     # Перемещаем окно по координатам курсора
-    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, x-350, y-350, 0, 0, win32con.SWP_NOSIZE)
+    win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, x-int(Window_Size/2), y-int(Window_Size/2), 0, 0, win32con.SWP_NOSIZE)
     
 
 def get_loudness(stream):
@@ -25,47 +58,81 @@ def get_loudness(stream):
 
 py.init() 
 
-window_screen = py.display.set_mode((700, 700),py.NOFRAME)
+window_screen = py.display.set_mode((Window_Size, Window_Size),py.NOFRAME)
 hwnd = py.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(
                        hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(255, 0, 128), 0, win32con.LWA_COLORKEY)
 
-character = py.image.load("Sprites/PWGood1.png")
-mouth5 = py.image.load("Sprites/PWGood6.png")
-mouth4 = py.image.load("Sprites/PWGood5.png")
-mouth3 = py.image.load("Sprites/PWGood4.png")
-mouth2 = py.image.load("Sprites/PWGood3.png")
-mouth1 = py.image.load("Sprites/PWGood2.png")
-mouth=[character,mouth1,mouth2,mouth3,mouth4,mouth5]
+# Поддерживаемые форматы изображений
+supported_formats = {".bmp", ".jpg", ".jpeg", ".png", ".gif"}
+
+# Получаем список файлов в директории и сортируем их по имени
+sprite_files = sorted([f for f in os.listdir(sprite_folder) if os.path.splitext(f)[1].lower() in supported_formats])
+# Загружаем и масштабируем изображения
+mouth = [py.transform.scale(py.image.load(os.path.join(sprite_folder, file)), (Window_Size, Window_Size)) for file in sprite_files]
+# Поворачиваем картинки. Для изменения инвертируйте Reflect.
+mouth = [py.transform.flip(picture, Reflect, False) for picture in mouth]
+
 
 close = 0
-countframes=0
-old=0
+
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
-device_index = None
-for i in range(p.get_device_count()):
-    device_info = p.get_device_info_by_index(i)
-    #print(device_info)
-    if "VoiceMeeter" in device_info["name"]:
-        device_index = i
-        break
-
-if device_index is None:
-    print("Не удалось найти зацикленное звуковое устройство.")
-    exit()
+# Настройки аудипотока. 
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-# создание входного потока аудио
-#stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, input_device_index=device_index, frames_per_buffer=CHUNK_SIZE)
-maxV=2000
+CHANNELS = 1 
+RATE = 48000
+
+# Поиск звукового потока (микрофон или другой источник через VoiceMeeter).
+# Для работы с уже записанными файлами (видео или музыка), настройте VoiceMeeter и активируйте режим VoiceMeeterMode.
+# VoiceMeeterMode особенно полезен для создания ИИ-каверов с персонажами. (Как пример)
+# После настройки VoiceMeeter просто запустите воспроизведение медиафайла в вашем стандартном медиаплеере.
+device_info={}
+if VoiceMeeterMode == True:
+    device_index = None
+    for i in range(p.get_device_count()):
+        device_info = p.get_device_info_by_index(i)
+        LogLine(False, False, True)
+        if "VoiceMeeter" in device_info["name"]:
+            device_index = i
+            break
+
+    if device_index is None:
+        print("Не удалось найти зацикленное звуковое устройство. Вероятно вы не установили VoiceMeeter")
+        exit()
+    
+    # создание входного потока аудио
+    try:
+        stream = p.open(format=FORMAT, channels=2, rate=RATE, input=True, input_device_index=device_index, frames_per_buffer=CHUNK_SIZE)
+    except Exception:
+        e = sys.exc_info()[1]
+        if(e.args[0]==-9999):
+            print("Надо настроить VoiceMetter. Включить VoiceMetter Input в настройках Звука.")
+        elif(e.args[0]==-9998):
+            print("Надо настроить VoiceMetter. Включить VoiceMetter Output в настройках Звука.")
+            print(f"Убедитесь, что количество каналов в настройках VoiceMetter не меньше, чем значение CHANNELS. Сейчас у вас CHANNELS = {CHANNELS}")
+        print(e.args)
+    
+
+else:
+    # Должно работать с микрофоном по умолчанию. 
+    try:
+        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+    except Exception:
+        e = sys.exc_info()[1]
+        if(e.args[0]==-9996):
+            print("У вас нет микрофона или он отключен в настройках звука.")
+        elif(e.args[0]==-9998):
+            print(f"Убедитесь, что количество каналов в настройках микрофона не меньше, чем значение CHANNELS. Сейчас у вас CHANNELS = {CHANNELS}")
+        print(e.args)
+
+
+
 t=time.time()
 window_screen.fill((255,0,128))
-window_screen.blit(character, (50, 50))
+window_screen.blit(mouth[0], (0, 0))
 while not close:
     for event in py.event.get():
         if event.type == py.QUIT:
@@ -83,10 +150,10 @@ while not close:
     if(loudness>maxV):
         loudness=maxV
     
-    Number_mouth=round(loudness/maxV*5)
+    Number_mouth=round(loudness/maxV * (len(mouth)-1))
     print(Number_mouth)
     for i in range(0,len(mouth)):
-        window_screen.blit(mouth[Number_mouth], (50, 50))
+        window_screen.blit(mouth[Number_mouth], (0, 0))
         if(Number_mouth==i):
             mouth[i].set_alpha(0)
         else:
